dd11b1828775ec8b550057d4a430a67308bcb561
enjoy <934389697@qq.com>
2026-04-28 22:46:22 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,19 +5,19 @@ import torch
 # -------------------------
 # Training parameters
 # -------------------------
-batch_size = 1
-ctx_len = 1024          # context length
+batch_size = 16
+ctx_len = 2048          # context length
 eval_interval = 20
 grad_accum = 8
 max_grad_norm=1.0
 
-lr = 1e-5
+lr = 1.5e-4
 min_lr = 2e-7
-dropout = 0.2
+dropout = 0.05
 
-max_iters = 1000
-eval_iters = 2
-warmup_iters = 500
+max_iters = 10000
+eval_iters = 20
+warmup_iters = 200
 
 resume = True
 res_path = ""
@@ -32,14 +32,14 @@ info_levl=1 #1:model parameters and Optimizer and  MoE / Router Info and mhc / m
 n_embd = 768
 n_head = 16
 n_layer = 12
-n_experts = 8
-num_exp=1
+n_experts = 32
+num_exp=2
 shared_experts=1
 use_expert_bias = 'True'
 types = ['moe']
-attention_types=['Spares']
+attention_types=['Sparse']
 hc=True
-vocab_size = 151643
+vocab_size = 32768
 init_moe_scaling = 1.25
 
 device = 'cuda' 
@@ -51,7 +51,7 @@ num_tokens_to_keep=128
 
 window_size=128
 
-weight_decay=1e-1
+weight_decay=1e-2
 
 bias=False
 
@@ -66,6 +66,7 @@ mhc = True
 sinkhorn_iters = 10
 sinkhorn_tau = 0.05
 mhc_h_res_proj = 'sinkhorn'
+cosine_decay = True
 
 # -------------------------
 # Neural Sort / Regularization
@@ -73,12 +74,12 @@ mhc_h_res_proj = 'sinkhorn'
 ns_steps = 5
 ns_eps = 1e-7
 ns_coeffs = (3.0, -3.2, 1.2)
-v_residual = False
+v_residual = True
 # NSA Branch Configuration
 nsa_use_branch1 = 1   # Coarse-grained compression (MLA)
-nsa_use_branch2 = 1   # Token selection (NSA)
-nsa_use_branch3 = 1   # Sliding window (NSA)
-attention_mode=['SWA','NSA','DSA']
+nsa_use_branch2 = 1   # Token selection (DSA)
+nsa_use_branch3 = 1   # Sliding window (SWA)
+attention_mode=['SWA','SWA','DSA','MLA','DSA','MLA','MLA','DSA','DSA','DSA','DSA','MLA']
 
 
 
